31dc191c0f63db44f30c52c8ee2f6fb4245bfbbd
mohammadsadegh.zarei <mohammadsadegh.zarei@partsoftware.com>
2026-09-16 16:40:24 +0330

    make new project and config db

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { createObserveModule } from '@nestjs/observe';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config'
+
+export const { ObserveModule, ObserveInstrument } = createObserveModule();
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      host: process.env.DB_HOST,
+      synchronize: true,
+      autoLoadEntities: true
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
